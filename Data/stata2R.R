e4795878b305fb4af48e96b6a4fe91e697aa965f
Jake Bowers <jake@jakebowers.org>
2019-11-20 16:02:58 -0300

    Updating in preo for outcome  analysis

--- Data/stata2R.R
+++ Data/stata2R.R
@@ -2,7 +2,7 @@ library(readstata13)
 library(here)
 
 #finaldat <- read.dta13(here("Data", "Final_database.dta"))
-finaldat <- read.dta13(here("Data", "basefinal1718.dta"))
+finaldat <- read.dta13(here("Data", "wd_basefinal1718.dta"))
 save(finaldat,file="finaldat.rda")
 
 
